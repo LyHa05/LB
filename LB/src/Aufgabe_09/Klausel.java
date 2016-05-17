5ed1bcaffa910fad107b34f1fa98d15f23f12aa8
Lydia <lydia.pflug@web.de>
2016-05-17 22:36:05 +0200

    Anfang

---
+++ LB/src/Aufgabe_09/Klausel.java
@@ -0,0 +1,12 @@
+package Aufgabe_09;
+
+import java.util.Set;
+
+public class Klausel {
+
+	private Set<Literal> klausel;
+	
+	public Klausel(Set klausel) {
+		this.klausel = new HashSet<Literal>();
+	}
+}
